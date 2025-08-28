ffccfa81f9719e29787156e3c6c3ed17f575e5eb
katakada <katakada@student.42tokyo.jp>
2025-08-29 00:25:40 +0900

    no horde

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,11 +6,11 @@
 Zombie *newZombie(std::string name);
 void randomChump(std::string name);
 
-std::string intToString(int value) {
-    std::stringstream ss;
-    ss << value;
-    return ss.str();
-}
+// std::string intToString(int value) {
+//     std::stringstream ss;
+//     ss << value;
+//     return ss.str();
+// }
 
 const int NUM_ZOMBIES = 10000;
 
@@ -23,14 +23,13 @@ int main() {
     zombie1->announce();
     delete zombie1;
 
-    Zombie *zombie[NUM_ZOMBIES];
+    // Zombie *zombie[NUM_ZOMBIES];
 
-    for (int i = 0; i < NUM_ZOMBIES; i++)
-        zombie[i] = newZombie(z_name + intToString(i));
-    for (int i = 0; i < NUM_ZOMBIES; i++)
-        zombie[i]->announce();
-    for (int i = 0; i < NUM_ZOMBIES; i++)
-        delete zombie[i];
+    // for (int i = 0; i < NUM_ZOMBIES; i++)
+    //     zombie[i] = newZombie(z_name + intToString(i));
+    // for (int i = 0; i < NUM_ZOMBIES; i++)
+    //     zombie[i]->announce();
+    // delete[] zombie;
 
     z_name = "stack_zombie";
     // for stack allocation
