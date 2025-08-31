1ce28d59fe371f719cc60507b98930da963d79ea
katakada <katakada@student.42tokyo.jp>
2025-08-31 22:24:22 +0900

    Fix formatting in Harl class output messages for better readability

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -24,31 +24,32 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug(void) {
-    std::cout
-        << "[ DEBUG ]\n"
-        << "I love having extra bacon for my "
-           "7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"
-        << std::endl;
+    std::cout << "[ DEBUG ]\n"
+              << "I love having extra bacon for my "
+                 "7XL-double-cheese-triple-pickle-specialketchup burger.\nI "
+                 "really do!\n"
+              << std::endl;
 }
 
 void Harl::info(void) {
     std::cout << "[ INFO ]\n"
-              << "I cannot believe adding extra bacon costs more money. You "
+              << "I cannot believe adding extra bacon costs more money.\nYou "
                  "didn’t put enough bacon in my burger! If you did, I wouldn’t "
-                 "be asking for more!"
+                 "be asking for more!\n"
               << std::endl;
 }
 
 void Harl::warning(void) {
     std::cout
         << "[ WARNING ]\n"
-        << "I think I deserve to have some extra bacon for free. I’ve been "
-           "coming for years, whereas you started working here just last month."
+        << "I think I deserve to have some extra bacon for free.\nI’ve been "
+           "coming for years, whereas you started working here just last "
+           "month.\n"
         << std::endl;
 }
 
 void Harl::error(void) {
     std::cout << "[ ERROR ]\n"
-              << "This is unacceptable! I want to speak to the manager now."
+              << "This is unacceptable! I want to speak to the manager now.\n"
               << std::endl;
 }
